cb366fd24a8018614adaeabd9e1090bc71087856
Tarahrr <hariri_tara@ut.ac.ir>
2016-06-20 21:33:11 +0430

    index.html - home page
    ۱-جلسه شروع کار وب (امید و تارا) برای  template  اولیه سایت
    ۲-ساختن صفحه ابتدایی سایت - منو بار و سلایدر
    ۳-اضافه کردن app.js  برای یادگیری  angular

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,10 @@
+(function(){
+
+	var app = angular.module('stock', []);
+
+	app.controller('StockController', ['$http', function($http){
+		var stockCtrl = this;
+		this.users =[]
+		
+	}]);
+})();
