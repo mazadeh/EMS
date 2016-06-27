e09a54d2d0897e9c38440016d1eac02502b0f08c
Nadiaghobadi <nadia.ghobadipasha@gmail.com>
2016-06-27 21:11:41 +0430

    announcement

--- src/database/dbo.Announcement.sql
+++ src/database/dbo.Announcement.sql
@@ -1,7 +1,12 @@
-CREATE TABLE [dbo].[Announcement](
-[ID] INT,
-[CourseName] NCHAR(20),
-[Deadline] DATE,
-[Content] TEXT,
-[Sendet] TEXT
-)
+CREATE TABLE announcement(
+	Content varchar(50) not null,
+	id integer not null,
+	Data varchar(10) not null,
+	Sender varchar(20) not null,
+);	
+INSERT INTO announcement(Content, ID, Data, Sender) VALUES ('content', 1, 'data', 'sender');
+SELECT * FROM announcement;
+UPDATE announcement SET Content='content' WHERE id=1;
+UPDATE announcement SET Data='data' WHERE id=1;
+UPDATE announcement SET Sender='sender' WHERE id=1;
+DELETE FROM announcement where id=1;
