a42bfdc051d096eae307bc046945dbef1f81a5a4
Tarahrr <hariri_tara@ut.ac.ir>
2016-06-28 11:24:55 +0430

    profile.html add to all of users
    تغییراتی که در اینجا رخ داده است به تمامی user ها اضافه شد همچنین تمامی
    فایل های html  را نیز در پوشه ایی با ترتیب تقسیم کردم.
    در صفحه پروفایل منو بار تغییر کرده و از drop down  استفاده شده است.

--- src/main/webapp/js/StyleJS.js
+++ src/main/webapp/js/StyleJS.js
@@ -25,4 +25,23 @@ $( document ).ready(function() {
         }
     ); 
 });// JavaScript Document
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip({
+        placement: 'top'
+    });   
+/* end dot nav */
+});
+
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
 //*********************
