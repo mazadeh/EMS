11cfe7d706000e132216883408341a3b8a4f8485
Mohammad Azadeh <m.azadeh@ut.ac.it>
2016-06-29 13:51:56 +0430

    SH files added for Linux users
    Using sh files speed up building, deploying, running, and testing the App.
    HSQLDB.jar added for SQL needs.

---
+++ bat-sh/hsqldb-startup.sh
@@ -0,0 +1,15 @@
+cd ../src/database
+
+if [ -d data-log ]
+then
+	echo Database exists
+else
+	echo Database created
+	mkdir -p data-log
+fi
+cd data-log
+
+echo 'New Run' >> hsqldb.log
+date >> hsqldb.log
+echo 'Server is running in the background...'
+java -cp ../hsqldb.jar org.hsqldb.server.Server >> hsqldb.log
